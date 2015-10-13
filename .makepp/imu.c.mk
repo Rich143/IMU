aa8a609a0762ff5a00d1af7bca4b86fd2efa0aa0
Rich143 <richard.arnold.matthews@gmail.com>
2015-10-13 19:03:41 -0400

    removed .makepp files

--- .makepp/imu.c.mk
+++
@@ -1,6 +0,0 @@
-INCLUDES=Accelerometer/Accelerometer.h Gyroscope/Gyroscope.h IMU.h Magnetometer/Magnetometer.h TWI/TWI.h Timer/Timer.h Uart/uart.h
-SYSTEM_INCLUDES=stdio.h
-C_MD5_SUM=Wxdx9GkQ9Kmoe7ShrvxJyw
-LIBS=
-SIGNATURE=1444776637,3796
-END=
--- Timer/.makepp/timer.c.mk
+++
@@ -1,6 +0,0 @@
-INCLUDES=Timer/Timer.h common.h
-SYSTEM_INCLUDES=avr/interrupt.h avr/io.h
-C_MD5_SUM=5Rd7wK8ueFcj1jOhdzUUmg
-LIBS=
-SIGNATURE=1444776637,646
-END=
